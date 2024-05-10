a38047d84fadd41931b3583b79930fccd1000d0e
Velaskits <velascomateoalex@gmail.com>
2024-05-10 20:20:31 +0200

    Enemigo que se hinmola

---
+++ Assets/Scripts/GeneradorEnemics.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneradorEnemics : MonoBehaviour
+{
+    public GameObject _prefabEnemic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("GeneraEnemic", 1, 2);
+    }
+
+    private void GeneraEnemic(){
+        GameObject enemic = Instantiate(_prefabEnemic);
+        enemic.transform.position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
